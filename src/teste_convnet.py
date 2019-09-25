968ca82cf1e452baf6293cbc7e6d0af58d81b3ed
OsvaldoCorreia <osvaldo.correia16@gmail.com>
2019-09-24 23:47:14 -0300

    erros sintaxe & myfile.csv

--- src/teste_convnet.py
+++ src/teste_convnet.py
@@ -5,10 +5,10 @@ import convnet
 print("Inicio do teste:")
 
 [X_treino, X_teste] = imal.image_analyser()
-df_y = pd.read_csv('myfile.csv', sep=',',header=None, usecols=[2])
+df_y = pd.read_csv("arq_teste\\teste01\\myfile.csv", sep=',',header=None, usecols=[2])
 y_treino, y_teste = df_y[:74], df_y[74:]
 
-convnet(X_treino, y_treino, X_teste, y_teste, 5, 369)
+convnet.convnet(X_treino, y_treino, X_teste, y_teste, 5, 369)
 
 print("Tamanho do X_treino: ",len(X_treino))
 print("Tamanho do X_teste: ",len(X_teste))
